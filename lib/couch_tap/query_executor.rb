baaee43f2daea7edce377df7a6ac085f8aa1c0d3
Carlos Alonso <carlos.alonso@cabify.com>
2017-03-27 11:06:20 +0200

    query executor integrated

--- lib/couch_tap/query_executor.rb
+++ lib/couch_tap/query_executor.rb
@@ -4,10 +4,8 @@ module CouchTap
 
     attr_reader :database
 
-    def connect(db, name)
-      raise "Cannot have two databases for a changeset!!" if database
+    def initialize(db)
       @database = Sequel.connect(db)
-      find_or_create_sequence_number(name)
     end
 
     def insert(db, attributes)
@@ -18,18 +16,19 @@ module CouchTap
       database[db].where(filter).delete
     end
 
-    def update_sequence(name, seq)
-      database[:couch_sequence].where(:name => name).update(:seq => seq)
+    def update_sequence(seq)
+      database[:couch_sequence].where(:name => @name).update(:seq => seq)
       return seq
     end
 
-    private 
-
     def find_or_create_sequence_number(name)
+      @name = name
       create_sequence_table(name) unless database.table_exists?(:couch_sequence)
       row = database[:couch_sequence].where(:name => name).first
       return (row ? row[:seq] : 0)
     end
+
+    private 
 
     def create_sequence_table(name)
       database.create_table :couch_sequence do
