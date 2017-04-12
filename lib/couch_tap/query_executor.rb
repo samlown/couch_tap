4425269ed4b110029a7d6a935bc9e427b560cf51
Carlos Alonso <carlos.alonso@cabify.com>
2017-04-12 14:09:59 +0200

    empty batches are skipped

--- lib/couch_tap/query_executor.rb
+++ lib/couch_tap/query_executor.rb
@@ -69,6 +69,10 @@ module CouchTap
     private
 
     def run_transaction(seq)
+      if @buffer.size == 0
+        logger.info "Skipping empty batch"
+        return
+      end
       if @buffer.size < @batch_size
         # Transaction was fired by the timer
         return if (Time.now - @last_transaction_ran_at) < @timeout_time
