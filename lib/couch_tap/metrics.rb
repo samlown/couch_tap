ed42719f628e7da37097f4c36c1b028215229ca2
Carlos Alonso <carlos.alonso@cabify.com>
2017-04-18 10:17:12 +0200

    building tags properly

--- lib/couch_tap/metrics.rb
+++ lib/couch_tap/metrics.rb
@@ -9,12 +9,24 @@ module CouchTap
       @tags = tags
     end
 
-    def increment(key, tags = {})
-      @statsd.increment(key, tags: tags.merge(@tags))
+    def increment(key, value = 1, tags = {})
+      if value == 1
+        @statsd.increment(key, tags: build_tags(tags))
+      else
+        @statsd.count(key, value, tags: build_tags(tags))
+      end
     end
 
     def histogram(key, value, tags = {})
-      @statsd.histogram(key, value, tags: tags.merge(@tags))
+      @statsd.histogram(key, value, tags: build_tags(tags))
+    end
+
+    private
+
+    def build_tags(tags)
+      @tags.merge(tags).map do |k, v|
+        "#{k}:#{v}"
+      end
     end
   end
 end
