aaf096fea19fb375e2c39ea09b28dde7d34b444a
Carlos Herrera <carlos.herrera@cabify.com>
2017-04-27 10:56:51 +0200

    Refactor and improve integration tests

--- test/integration/couch_tap_test.rb
+++ test/integration/couch_tap_test.rb
@@ -3,6 +3,16 @@ require 'test_helper'
 
 class CouchTapIntegrationTest < Test::Unit::TestCase
   DB_FILE = "integration.sqlite"
+  DUMMY_ANALYTIC_EVENT = { type: "AnalyticEvent", user_id: "6j26n146"}
+  DUMMY_SALE = { type: "Sale",
+                code: "R", amount: 20, entries: [ { price: 10.0 },
+                                                  { price: 20.0 },
+                                                  { price: 30.0 },
+                                                  { price: 40.0 },
+                                                  { price: 50.0 },
+                                                  { price: 60.0 },
+                                                  { price: 70.0 }]}
+  DUMMY_UPDATE_FIELDS = { amount: 200, entries: (1..7).map { |i| { price: 100.0 * i } } }
 
   def setup
     reset_test_db!
@@ -18,7 +28,59 @@ class CouchTapIntegrationTest < Test::Unit::TestCase
   end
 
   def test_insert_update_delete
-    config = <<-CFG
+    CouchTap.module_eval(config)
+
+    th = Thread.new { CouchTap.start }
+    th.abort_on_exception = true
+
+    Timecop.freeze(Time.now.round - 120)
+      TEST_DB.save_doc(DUMMY_SALE)
+      sleep 0.1
+      assert_equal 1, @db[:sales].count
+      assert_equal 1, @db[:couch_sequence].where(name: TEST_DB_NAME).first[:seq]
+      sale = @db[:sales].first
+      assert_equal_sale(DUMMY_SALE, sale)
+    Timecop.return
+
+    Timecop.freeze(Time.now)
+      docu = TEST_DB.update_doc sale[:sale_id] do |doc|
+        doc[:amount] = DUMMY_UPDATE_FIELDS[:amount]
+        doc[:entries] = DUMMY_UPDATE_FIELDS[:entries]
+      end
+      sleep 0.1
+      assert_equal 1, @db[:sales].count
+      assert_equal 2, @db[:couch_sequence].where(name: TEST_DB_NAME).first[:seq]
+      expected_updated_sale = DUMMY_SALE.merge(DUMMY_UPDATE_FIELDS)
+      updated_sale = @db[:sales].first
+      assert_equal sale[:sale_id], updated_sale[:sale_id]
+      assert_equal_sale expected_updated_sale, updated_sale
+    Timecop.return
+
+    TEST_DB.delete_doc(docu)
+
+    sleep 1 # Sleep to allow the timer to run
+
+    assert_equal 0, @db[:sales].count
+    assert_equal 3, @db[:couch_sequence].where(name: TEST_DB_NAME).first[:seq]
+
+    CouchTap.stop
+    TEST_DB.save_doc(dummy: true) # This is a HACK to make the client disconnect from the feed.
+    th.join
+  end
+
+  private
+
+  def assert_equal_sale(expected_sale, db_sale)
+    assert_equal expected_sale[:code], db_sale[:code]
+    assert_equal expected_sale[:amount], db_sale[:amount]
+    assert_equal Time.now, db_sale[:audited_at]
+    entries = @db[:sale_entries].where(sale_id: db_sale[:sale_id]).to_a
+    assert_equal expected_sale[:entries].size, entries.count
+    expected_sale[:entries].each { |entry| assert_includes entries, price: entry[:price], sale_id: db_sale[:sale_id] }
+  end
+
+  def config
+    <<-CFG
       changes couch_db: TEST_DB_ROOT, timeout: 0.5 do
         database db: "sqlite://#{DB_FILE}", batch_size: 10
 
@@ -39,60 +101,7 @@ class CouchTapIntegrationTest < Test::Unit::TestCase
         end
       end
     CFG
-
-    CouchTap.module_eval(config)
-    th = Thread.new do
-      CouchTap.start
-    end
-    th.abort_on_exception = true
-
-    TEST_DB.save_doc(type: "Sale", code: "R", amount: 20, entries: [ { price: 10.0 }, { price: 20.0 }, { price: 30.0 }, { price: 40.0 }, { price: 50.0 }, { price: 60.0 }, { price: 70.0 }])
-
-    sleep 0.1
-
-    assert_equal 1, @db[:sales].count
-    sale = @db[:sales].first
-    assert_equal "R", sale[:code]
-    assert_equal 20, sale[:amount]
-    entries = @db[:sale_entries].where(sale_id: sale[:sale_id]).to_a
-    assert_equal 7, entries.count
-    (10..70).step(10) { |price| assert_includes entries, price: price, sale_id: sale[:sale_id] }
-    assert_equal 1, @db[:couch_sequence].where(name: TEST_DB_NAME).first[:seq]
-
-    docu = TEST_DB.update_doc sale[:sale_id] do |doc|
-      doc[:amount] = 200
-      doc[:entries] = [{ price: 100.0 }, { price: 200.0 }, { price: 300.0 }, { price: 400.0 }, { price: 500.0 }, { price: 600.0 }, { price: 700.0 }]
-    end
-
-    sleep 0.1
-
-    sale_bak = sale
-    assert_equal 1, @db[:sales].count
-    sale = @db[:sales].first
-    assert_equal sale_bak[:sale_id], sale[:sale_id]
-    assert_equal "R", sale[:code]
-    assert_equal 200, sale[:amount]
-    entries = @db[:sale_entries].where(sale_id: sale[:sale_id]).to_a
-    assert_equal 7, entries.count
-    (100..700).step(100) { |price| assert_includes entries, price: price, sale_id: sale[:sale_id] }
-    assert_equal 2, @db[:couch_sequence].where(name: TEST_DB_NAME).first[:seq]
-
-    TEST_DB.delete_doc(docu)
-
-    sleep 1 # Sleep to allow the timer to run
-
-    assert_equal 0, @db[:sales].count
-
-    CouchTap.stop
-
-    TEST_DB.save_doc(dummy: true) # This is a HACK to make the client disconnect from the feed.
-
-    th.join
-
-    assert_equal 3, @db[:couch_sequence].where(name: TEST_DB_NAME).first[:seq]
   end
-
-  private
 
   def setup_db(connection)
     connection.create_table :sales do
@@ -100,6 +109,7 @@ class CouchTapIntegrationTest < Test::Unit::TestCase
       String :code
       Float :amount
       Time :updated_at
+      Time :audited_at
     end
 
     connection.create_table :sale_entries do
