03cd629929959cb40183645b85300a52d3f72563
Carlos Alonso <carlos.alonso@cabify.com>
2017-06-26 17:39:48 +0200

    Throttling in case input is quicker than output in the queue

--- lib/couch_tap/operations_queue.rb
+++ lib/couch_tap/operations_queue.rb
@@ -3,13 +3,20 @@ module CouchTap
   class OperationsQueue
     extend Forwardable
 
+    MAX_QUEUE_SIZE = 100_000
+
     def_delegators :@queue, :length, :pop
 
-    def initialize
+    def initialize(max_length = MAX_QUEUE_SIZE, sleep_time = 1)
       @queue = Queue.new
+      @max_length = max_length
+      @sleep_time = sleep_time
     end
 
     def add_operation(op)
+      while @queue.length >= @max_length
+        sleep @sleep_time
+      end
       @queue.push op
     end
 
