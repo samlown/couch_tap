90bc453c33af05f8fe72d1ff7e922582f716c76c
Antonio Momblan <antonio.momblan@cabify.com>
2017-11-16 12:12:07 +0100

    Make usage of logstash optional

---
+++ lib/couch_tap/log.rb
@@ -0,0 +1,45 @@
+
+#require 'logging'
+require 'logging/logstash'
+
+module CouchTap
+  module LogFactory
+    LOG_PATTERN = "%.1l [%d] %5l - %c: %m\n"
+
+    def self.config_loggers()
+      layout = Logging.layouts.pattern(:pattern => LOG_PATTERN)
+      level = parse_level(ENV.fetch('LOG_LEVEL', 'info'))
+      Logging.appenders.stdout('stdout', 
+                               :level => level, 
+                               :layout => layout)
+      Logging.logger.root.add_appenders('stdout')
+      if ENV.fetch('USE_LOGSTASH', 'false') == "true"
+        configure_logstash()
+      end
+    end
+
+    def self.configure_logstash()
+      logstash_host = ENV.fetch('LOGSTASH_HOST', 'localhost')
+      logstash_port = ENV.fetch('LOGSTASH_PORT', '5016')
+      level = parse_level(ENV.fetch('LOGSTASH_LOG_LEVEL', 'debug'))
+      Logging.appenders.logstash('logstash', 
+                                 :level => level,
+                                 :layout => layout,
+                                 :uri => "udp://#{logstash_host}:#{logstash_port}")
+      Logging.logger.root.add_appenders('logstash')
+    end
+
+    def self.parse_level(level)
+      case level
+      when 'unknown' then :unknown
+      when 'fatal' then :fatal
+      when 'error' then :error
+      when 'warn' then :warn
+      when 'info' then :info
+      when 'debug' then :debug
+      else
+        raise "Unknown log level: #{level}"
+      end
+    end
+  end
+end
